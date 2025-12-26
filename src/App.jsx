1b7003481fd43d59aaee5da6368e64cdb3508a61
Rism583 <risme1522@gmail.com>
2025-12-26 23:52:01 +0530

    Property gallery connected in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import Header from './Components/Header.jsx';
+import Navbar from './Components/Navbar.jsx';
+import Footer from './Components/Footer.jsx';
+import PropertyGallery from './Components/PropertyGallery.jsx';
 import './App.css'
 
 function App() {
   return (
     <div className="app-container">
-      
+      <Header />
+      <Navbar />
+
+      <main>
+        <PropertyGallery />
+      </main>
+
+      <Footer />
     </div>
   )
 }
