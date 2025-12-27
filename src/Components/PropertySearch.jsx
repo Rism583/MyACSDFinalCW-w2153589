6300dcc4c201434dc824dc713e300b14cc9e0a2d
Rism583 <risme1522@gmail.com>
2025-12-28 00:45:15 +0530

    search properties by type is implemented

---
+++ src/Components/PropertySearch.jsx
@@ -0,0 +1,37 @@
+import './Styles/PropertySearch.css';
+import React from 'react';
+
+const PropertySearch = ({filter, setFilter}) => {
+
+    //Handler for filter input changes
+    const handlefilterChange = (e) => {
+        const {name, value} = e.target;
+        
+        setFilter({
+            ...filter,
+            [name]: value
+        });
+    };
+
+    return (
+        <section className="search-section">
+            <h2>Search Properties</h2>
+            <div className="filter-container">
+                <label htmlFor="type">Property Type:</label>
+                <select id="type" name="type" value={filter.type} onChange={handlefilterChange}>
+                    <option value="any">Any</option>
+                    <option value="House">House</option>
+                    <option value="Cottage">Cottage</option>
+                    <option value="Duplex">Duplex</option>
+                    <option value="Flat">Flat</option>
+                    <option value="Bungalow">Bungalow</option>
+                    <option value="Townhouse">Townhouse</option>
+                </select>
+            </div>
+        </section>
+
+    )
+
+}
+
+export default PropertySearch;
