231c35196b153920255a9cede529bc84564669bc
Rism583 <risme1522@gmail.com>
2025-12-27 15:39:53 +0530

    postcode datas added in properties.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import Header from './Components/Header.jsx';
-import Navbar from './Components/Navbar.jsx';
-import Footer from './Components/Footer.jsx';
 import PropertyGallery from './Components/PropertyGallery.jsx';
 import './App.css'
+import React from 'react';
 
 function App() {
   return (
     <div className="app-container">
-      <Header />
-      <Navbar />
+      
 
       <main>
         <PropertyGallery />
       </main>
 
-      <Footer />
     </div>
   )
 }
