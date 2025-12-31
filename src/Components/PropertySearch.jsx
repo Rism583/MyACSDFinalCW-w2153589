534fd3cd85c3bc9c0872c4d827833f6530753346
Rism583 <risme1522@gmail.com>
2025-12-31 23:00:11 +0530

    propertysearch has been updated to apply type widget

--- src/Components/PropertySearch.jsx
+++ src/Components/PropertySearch.jsx
@@ -2,6 +2,8 @@ import './Styles/PropertySearch.css';
 import React from 'react';
 import DateWidget from './DateWidget';//importing DateWidget component for date inputs
 import RangeWidget from './RangeWidget';//importing RangeWidget component for range inputs
+import TypeWidget from './TypeWidget';//importing TypeWidget component for type selection
+
 const PropertySearch = ({filter, setFilter}) => {
 
     //Handler for filter input changes
@@ -23,18 +25,11 @@ const PropertySearch = ({filter, setFilter}) => {
             <h2>Search Properties</h2>
 
             {/*Dropdown for property type filter*/}
-            <div className="filter-container">
-                <label htmlFor="type">Property Type:</label>
-                <select id="type" name="type" value={filter.type} onChange={handlefilterChange}>
-                    <option value="any">Any</option>
-                    <option value="House">House</option>
-                    <option value="Cottage">Cottage</option>
-                    <option value="Duplex">Duplex</option>
-                    <option value="Flat">Flat</option>
-                    <option value="Bungalow">Bungalow</option>
-                    <option value="Townhouse">Townhouse</option>
-                </select>
-            </div>
+            <TypeWidget
+                label="Property Type:"
+                value={filter.type}
+                onChange={handlefilterChange}
+            />
 
             {/*Input for postcode filter*/}
             <div className="filter-container">
