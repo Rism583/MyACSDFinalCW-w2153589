7a61b6af056bb8288054ca4acf4163dbc0cbc320
Rism583 <risme1522@gmail.com>
2026-01-01 21:30:21 +0530

    Header and navbar added for the applicaton

---
+++ src/Components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from 'public/Applogo.png';
+
+const Header = () => {
+    return (
+
+        //main header container
+        <header className="app-header">
+
+            {/* Logo and Title Section */}
+            <div className="logo-container">
+                <img src={logo} alt="Simply Home App Logo" className="app-logo" />
+                <h1 className="app-title">Simply Home</h1>
+                <p className="app-tagline">Your destination, Beautifully Discovered...</p>
+            </div>
+
+            {/* Navigation Links Section */}
+            <nav className="navigation-bar">
+
+                {/* Navigation Links using NavLink for active link styling */}
+                <NavLink to="/" className={({ isActive }) => isActive ? 'nav-link active-link' : 'nav-link'}>
+                    Home
+                </NavLink>
+
+                <NavLink to="/properties" className={({ isActive }) => isActive ? 'nav-link active-link' : 'nav-link'}>
+                    Properties
+                </NavLink>
+
+                <div className="nav-favourite-count" >
+                    <NavLink to="/favourites" className={({ isActive }) => isActive ? 'nav-link active-link' : 'nav-link'}>
+                        Favourites
+                    </NavLink>
+                </div>
+
+            </nav>
+        </header>
+    );
+}
+export default Header;
+
+            
