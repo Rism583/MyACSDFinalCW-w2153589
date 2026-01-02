330c2237d9caeaadb89f28b260153a77d2238f1f
Rism583 <risme1522@gmail.com>
2026-01-02 08:11:43 +0530

    styling for propert gallery and search added

--- src/Components/PropertyGallery.jsx
+++ src/Components/PropertyGallery.jsx
@@ -125,94 +125,101 @@ const PropertyGallery = ({ properties, favourites, addToFavourites, removeFromFa
         <div className="property-gallery">
             <h1>{viewMode === "search" ? "  Featured Properties" : "Property Gallery"}</h1>
 
-            {/* Property Search Component - rendered only in search view mode */}
-            {viewMode === "search" && (
-                <PropertySearch filter={filter} setFilter={setFilter} />
-            )}
+            <div className='gallery-layout'>
+                {/* Property Search Component - rendered only in search view mode */}
+                {viewMode === "search" && (
+                    <aside className="sidebar-container"> 
+                        <PropertySearch filter={filter} setFilter={setFilter} />
+                        {/* Toggle Favourites Button */}
+                        <button className="view-fav-btn" 
+                            onClick={() => setShowFavourites(!showFavourites)}
+                            onDragOver={handleDragOver} // Allow drag over
+                            onDrop={handleDrop}> {/* Handle drop event */}
+                            {showFavourites ? 'Hide Favourites' : 'View Favourites'} ({favourites.length})
+                        </button>
+                    </aside>
+                
+                )}
 
+                <main className='main-content'>
+                    {/* Favourites Section */}
+                    {showFavourites ? (
+                        <div className="favorites-container"
+                        onDragOver={handleDragOver}
+                        onDrop={handleDrop}>
+                            <h2>My Favourites ({favourites.length})</h2>
+                            {favourites.length === 0
+                                ? <p className="no-favs-msg">No Favourite prperties added yet</p>
+                                : <div className="favs-list">
+
+                                    {/* Mapping through the favourites array to display each favourite property */}
+                                    {favourites.map((favourite) => (
+                                        <div className="favourite-card" key={favourite.id}>
+
+                                            {/* Favourite Property Image and Details Section */}
+                                            <img src ={favourite.picture} alt={favourite.type} className="fav-prop-img "/>
+
+                                            <div className="fav-prop-details">
+                                                <h3>{favourite.type} with {favourite.bedrooms} bedrooms</h3>
+                                                <h4>£{favourite.price.toLocaleString()}</h4>
+                                            </div>
+
+                                            {/* Remove from favourites button */}
+                                            <button 
+                                                className="remove-fav-button"
+                                                onClick={() => removeFromFavourites(favourite.id)}>                                
+                                                Remove from Favourites
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            }
+                            {/* Clear all favourites button */}
+                            {favourites.length > 0 && (
+                                <button className="clear-favs-button" onClick={clearFavourites}>
+                                    Clear All Favourites
+                                </button>
+                            )}
+                        </div>
+                    //show only the filtered properties section
+                    ): (<div className='property-grid-container'>
+                            {filteredProperties.length > 0 ? (
+                                <div className="property-grid">
+                                    {/* Mapping through the properties array to create PropertyCard components */}  
+                                    {filteredProperties.map((item) => (
+                                        <PropertyCard
+                                            key={item.id} //unique key for each property 
+                                            property={item} //passing one entire property data into the PropertyCard
+                                            addToFavourites={addToFavourites} //passing the addToFavourites function as a prop
+                                            isFavourite={favourites.some((fav) => fav.id === item.id)} //checking if the property is in favourites
+                                        />
+                                ))}
+                                </div>
+                            ) : (
+                                <div className="no-results-msg">
+                                    <h4>No properties match your current filters.</h4>
+                                    <p>Please adjust your filter criteria and try again.</p>
+                                    <button className='reset-filters-btn' onClick={() => {
+                                        setFilter({
+                                            type: 'any',
+                                            minPrice: "No Min",
+                                            maxPrice: "No Max",
+                                            minBedrooms: "No min",
+                                            maxBedrooms: "No max",
+                                            startDate: '2000-01-31',
+                                            endDate: '2026-12-31',
+                                            postcode: ''
+                                        });
+                                    }}>Reset Filters</button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </main>
+            </div>
             
 
-            {/* Toggle Favourites Button */}
-            <button className="view-fav-btn" 
-            onClick={() => setShowFavourites(!showFavourites)}
-            onDragOver={handleDragOver} // Allow drag over
-            onDrop={handleDrop}> {/* Handle drop event */}
-                {showFavourites ? 'Hide Favourites' : 'View Favourites'} ({favourites.length})
-            </button>
-
-            {/* Favourites Section */}
-            {showFavourites ? (
-                <div className="favorites-container"
-                onDragOver={handleDragOver}
-                onDrop={handleDrop}>
-                    <h2>My Favourites ({favourites.length})</h2>
-                    {favourites.length === 0
-                        ? <p className="no-favs-msg">No Favourite prperties added yet</p>
-                        : <div className="favs-list">
-
-                            {/* Mapping through the favourites array to display each favourite property */}
-                            {favourites.map((favourite) => (
-                                <div className="favourite-card" key={favourite.id}>
-
-                                    {/* Favourite Property Image and Details Section */}
-                                    <img src ={favourite.picture} alt={favourite.type} className="fav-prop-img "/>
-
-                                    <div className="fav-prop-details">
-                                        <h3>{favourite.type} with {favourite.bedrooms} bedrooms</h3>
-                                        <h4>£{favourite.price.toLocaleString()}</h4>
-                                    </div>
-
-                                    {/* Remove from favourites button */}
-                                    <button 
-                                        className="remove-fav-button"
-                                        onClick={() => removeFromFavourites(favourite.id)}>                                
-                                        Remove from Favourites
-                                    </button>
-                                </div>
-                            ))}
-                        </div>
-                    }
-                    {/* Clear all favourites button */}
-                    {favourites.length > 0 && (
-                        <button className="clear-favs-button" onClick={clearFavourites}>
-                            Clear All Favourites
-                        </button>
-                    )}
-                </div>
-            //show only the filtered properties section
-            ): (<div className='property-grid-container'>
-                    {filteredProperties.length > 0 ? (
-                        <div className="property-grid">
-                            {/* Mapping through the properties array to create PropertyCard components */}  
-                            {filteredProperties.map((item) => (
-                                <PropertyCard
-                                    key={item.id} //unique key for each property 
-                                    property={item} //passing one entire property data into the PropertyCard
-                                    addToFavourites={addToFavourites} //passing the addToFavourites function as a prop
-                                    isFavourite={favourites.some((fav) => fav.id === item.id)} //checking if the property is in favourites
-                                />
-                        ))}
-                        </div>
-                    ) : (
-                        <div className="no-results-msg">
-                            <h4>No properties match your current filters.</h4>
-                            <p>Please adjust your filter criteria and try again.</p>
-                            <button className='reset-filters-btn' onClick={() => {
-                                setFilter({
-                                    type: 'any',
-                                    minPrice: "No Min",
-                                    maxPrice: "No Max",
-                                    minBedrooms: "No min",
-                                    maxBedrooms: "No max",
-                                    startDate: '2000-01-31',
-                                    endDate: '2026-12-31',
-                                    postcode: ''
-                                });
-                            }}>Reset Filters</button>
-                        </div>
-                    )}
-                </div>
-            )}
+            
             {/* Loading message if properties are not yet loaded */}
             {properties.length === 0 && (<p>Loading properties...</p>)}
         </div>
