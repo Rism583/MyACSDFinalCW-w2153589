7b275f243d9d82ca0b58bcb4c91b3b69af8a5247
Rism583 <risme1522@gmail.com>
2026-01-04 17:06:25 +0530

    for postcode searching search widget has created

---
+++ src/Components/SearchWidget.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Styles/Widgets.css';  
+
+const SearchWidget = ({ label, name, value, onChange }) => { //SearchWidget component to handle search inputs
+    return (
+        <div className="search-widget-container">
+            <label htmlFor={name}>{label}</label>
+            {/* Search input field */}
+            <input
+                type="search"
+                id={name}
+                name={name}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className="search-input"
+            />
+        </div>
+    );
+}
+export default SearchWidget;
